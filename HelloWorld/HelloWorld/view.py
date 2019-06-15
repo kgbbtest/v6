363dd0a5c304f361c1df86d152d01a9d3a273dd7
kgbbtest <root@na892.com>
2019-06-15 13:50:13 +0800

    6667

--- HelloWorld/HelloWorld/view.py
+++ HelloWorld/HelloWorld/view.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 def hello(request):
     context          = {}
-    context['hello'] = 'He World!'
+    context['hello'] = 'He666 World!'
     return render(request, 'hello.html', context)
